ec3d846fd883883d8d7a42e8820af90ed6f37ead
Kairi <seamile.rivercape@gmail.com>
2017-06-25 20:55:09 +0900

    [modify] Modify configurations for CSS builder

--- postcss.config.js
+++ postcss.config.js
@@ -1,24 +1,6 @@
-const atImport = require('postcss-import');
-const cssnext = require('postcss-cssnext');
-
 module.exports = {
   plugins: {
-    atImport(),
-    cssnext({
-      browsers: [
-        // Specify supported browser-sync
-        // ==============================
-        // cf.) https://github.com/ai/browserslist#queries
-        'Chrome ESR',
-        'Edge ESR',
-        'Explorer ESR',
-        'Firefox ESR',
-        'Safari ESR',
-        'iOS ESR',
-        'Android >= 4.4',
-        'AndroidChrome ESR',
-        // '> 1% in JP', // Specify with two-letter country code
-      ]
-    })
+    'postcss-import': {},
+    'postcss-cssnext': {}
   }
 };
