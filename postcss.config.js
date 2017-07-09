fd640f031a47aab618baee8761e4e2693cf6955d
Kairi <seamile.rivercape@gmail.com>
2017-07-09 22:19:54 +0900

    [add] Load new modules

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,31 @@
 module.exports = {
   plugins: {
-    'postcss-import': {},
-    'postcss-cssnext': {}
+    'postcss-import': {
+      plugins: [
+        require('postcss-import-url'),
+      ]
+    },
+    'postcss-nesting': {},
+    'postcss-cssnext': {
+      autoprefixer: {
+        browsers: [
+          'last 2 Chrome versions',
+          'last 2 Edge versions',
+          'ie 11',
+          'last 2 Firefox versions',
+          'last 2 Safari versions',
+          'last 2 iOS versions',
+          'Android >= 4.4',
+          'last 2 ChromeAndroid versions'
+        ]
+      },
+      nesting: true
+    },
+    'css-declaration-sorter': {
+      order: 'smacss'
+    }
+    'postcss-reporter': {
+      clearMessages: true
+    }
   }
 };
--- posthtml.config.js
+++ posthtml.config.js
@@ -3,7 +3,21 @@ module.exports = (ctx) => {
     from: ctx.from,
     to: ctx.to,
     plugins: {
-      'posthtml-w3c': {}
+      'posthtml-w3c': {},
+      'posthtml-hint': {
+        // cf.) https://github.com/yaniswang/HTMLHint/wiki/Rules
+        'tagname-lowercase': true,
+        'attr-lowercase': true,
+        'attr-value-double-quotes': true,
+        'doctype-first': true,
+        'tag-pair': true,
+        'spec-char-escape': true,
+        'id-unique': true,
+        'src-not-empty': true,
+        'attr-no-duplication': true,
+        'title-require': true,
+        'head-script-disabled': true
+      }
     }
   }
 };
