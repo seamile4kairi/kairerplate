f18bf918278c6c10663065cf7c3648d11d555f96
Kairi <seamile.rivercape@gmail.com>
2022-01-09 16:09:44 +0900

    [add] Added Stimulus.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,7 +2,12 @@
  * Common
  * ========================================================================== */
 
-console.warn('Hello World!')
+import {
+  Application,
+} from '@hotwired/stimulus'
+
+const $app = Application.start()
+if (process.env.NODE_ENV !== 'production') window.$app = $app
 
 
 /**
